7f801701898dec12b578aafa35f7df8fa64f2c05
Kris Buytaert <sdog@inuits.be>
2012-02-07 22:13:34 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,37 @@
+# Class: statsd
+#
+# This module manages statsd
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class statsd (
+  $graphite_host = "localhost",
+  $graphite_port = 2003,
+  $statsd_port   = 8125
+) {
+
+  file { '/etc/statsd/config.js':
+    ensure  => 'file',
+    group   => '0',
+    mode    => '644',
+    owner   => '0',
+    content => template("statsd/config.js.erb"),
+    require => Package['statsd'],
+  }
+
+  package {"statsd":
+    ensure => present;
+  }
+  service { "statsd":
+    enable  => true,
+    require => File["/etc/statsd/config.js"]
+  }
+
+}
